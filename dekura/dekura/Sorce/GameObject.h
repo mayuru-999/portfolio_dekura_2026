a57ee48610fd322c1c633b36cdef870273268e40
dekura <>
2026-05-22 15:55:05 +0900

    siv3D導入

---
+++ dekura/dekura/Sorce/GameObject.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+class GameObject
+{
+public:
+	virtual ~GameObject() = 0;
+	virtual void Update() = 0;
+	virtual void Draw() = 0;
+	void destroy() {};
+};
---
+++ dekura/dekura/Sorce/Main.cpp
@@ -0,0 +1,19 @@
+﻿#include <Siv3D.hpp> // Siv3D v0.6.16
+#include "GameObject.h"
+
+void Main()
+{
+	Array<std::shared_ptr<GameObject>> objects;
+
+	while (System::Update())
+	{
+		for (auto& obj : objects)
+		{
+			obj->Update();
+		}
+		for (auto& obj : objects)
+		{
+			obj->Draw();
+		}
+	}
+}
